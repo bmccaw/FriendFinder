8c75d3890e60ce6dec55e20224306cdfb76fbb84
Green Man <brendan.mccaughey@gmail.com>
2018-12-14 12:29:26 -0500

    App connecting to Express server and am able to access HTML through the localhost.

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+  });
