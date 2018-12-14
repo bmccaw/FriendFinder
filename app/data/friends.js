3ed7377775d523ba5e6bd77cf2ffdbc145d41030
Green Man <brendan.mccaughey@gmail.com>
2018-12-14 14:58:11 -0500

    Basic functionality finished. Switched to Materialize instead of Bootstrap to try it out.

---
+++ app/data/friends.js
@@ -0,0 +1,123 @@
+var friendsArray = [
+    {
+        name: 'Larry',
+        photo: 'https://www.mariowiki.com/images/7/71/NSMBU_LarryKoopa.png',
+        scores: [
+                '1',
+                '2',
+                '4',
+                '4',
+                '5',
+                '1',
+                '1',
+                '2',
+                '3',
+                '2'
+        ]
+
+    },
+    {
+        name: 'Morton',
+        photo: 'https://www.mariowiki.com/images/d/d1/MortonNSMBU.png',
+        scores: [
+                '4',
+                '2',
+                '3',
+                '4',
+                '3',
+                '2',
+                '1',
+                '4',
+                '3',
+                '2'
+        ]
+
+    },
+    {
+        name: 'Wendy',
+        photo: 'https://www.mariowiki.com/images/b/bc/WendyNSMBU.png',
+        scores: [
+                '5',
+                '1',
+                '3',
+                '2',
+                '5',
+                '1',
+                '1',
+                '2',
+                '4',
+                '4'
+        ]
+
+    },
+    {
+        name: 'Iggy',
+        photo: 'https://www.mariowiki.com/images/9/97/IggyNSMBU.png',
+        scores: [
+                '3',
+                '2',
+                '3',
+                '2',
+                '5',
+                '2',
+                '1',
+                '2',
+                '5',
+                '1'
+        ]
+
+    },
+    {
+        name: 'Roy',
+        photo: 'https://www.mariowiki.com/images/b/bd/RoyNSMBU.png',
+        scores: [
+                '2',
+                '4',
+                '3',
+                '3',
+                '5',
+                '2',
+                '1',
+                '2',
+                '1',
+                '1'
+        ]
+
+    },
+    {
+        name: 'Lemmy',
+        photo: 'https://www.mariowiki.com/images/f/fe/Lemmy_Koopa_NSMBU.png',
+        scores: [
+                '1',
+                '5',
+                '5',
+                '2',
+                '5',
+                '1',
+                '3',
+                '2',
+                '3',
+                '5'
+        ]
+
+    },
+    {
+        name: 'Ludwig',
+        photo: 'https://www.mariowiki.com/images/8/8e/LudwigNSMBU.png',
+        scores: [
+                '2',
+                '2',
+                '2',
+                '5',
+                '5',
+                '1',
+                '3',
+                '1',
+                '3',
+                '1'
+        ]
+
+    }
+];
+
+module.exports = friendsArray;
