16a2e15b3344665d3b1192a78189a7696e4bd7b2
Green Man <brendan.mccaughey@gmail.com>
2018-12-14 19:14:09 -0500

    First styling pass.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//allow access to public dir for CSS
+app.use(express.static(path.join(__dirname, './app/public')));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +16,8 @@ app.use(express.json());
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
+
+//Check to make sure server is running
 app.listen(PORT, function() {
     console.log('Friend Finder app is listening on PORT: ' + PORT);
   });
